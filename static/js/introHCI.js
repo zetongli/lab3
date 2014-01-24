f7518108563615ceb7906de04c4402ab9e6858c2
vagrant <vagrant@precise32.(none)>
2014-01-24 21:52:17 +0000

    fixed toggle class

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,33 +10,44 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Try it");
-		$("#testjs").text("Welllllll......");
+	$('.jumbotron h1').text("Try it");
+	$("#testjs").text("Just changed");
 
 
-		$(".jumbotron p").toggleClass("active");
-
-
-
+	$(".jumbotron p").toggleClass("active");
 
 	});
+
+	
+	$("#submitBtn").click(updateProject);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 	$("a.thumbnail").click(projectClick);
 
+  $(".project").append("<div class='project-description'><p>Description of the project.</p></div>");
+  var descriptions = $(".project-description");
+  descriptions.addClass("displayToggle");
+
+
 
 	// $(".jumbotron p").addClass("active");
-
-
-
-	$("#submitBtn").click(updateProject);
-	
 }
+
+// function updateProject(e) {
+//    var projectID = $('#project').val();
+
+//    $(projectID).animate({
+//       width: $('#width').val()
+//    });
+
+//    var newText = $('#description').val();
+//    $(projectID + " .project-description").text(newText);
+// }
+
 
 function updateProject(e) {
    var projectID = $('#project').val();
-
    $(projectID).animate({
       width: $('#width').val()
    });
@@ -44,7 +55,6 @@ function updateProject(e) {
    var newText = $('#description').val();
    $(projectID + " .project-description").text(newText);
 }
-
 
 
 
@@ -76,25 +86,30 @@ function projectClick(e) {
     jumbotronHeader.text(projectTitle);
 
 
-    // var containingProject = $(this).closest(".project");
-    // containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
-
-	var containingProject = $(this).closest(".project");
+	  var containingProject = $(this).closest(".project");
     var description = $(containingProject).find(".project-description");
 
-    if (description.length == 0) {
-       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
-    	// $(description).fadeIn();
-    } else {
-       // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
-       // $(description).fadeOut();
-
-       // setTimeout($(description).remove,3000)
-
-       $(description).remove();
+    $(description).toggleClass("displayToggle");
 
 
-    }
+
+
+
+
+
+    // if (description.length == 0) {
+    // // $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+
+    // 	 $(description).fadeIn();
+
+    // } else {
+    //    // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       
+
+    //       $(description).fadeOut("slow",function(){
+    //       $(description).remove();
+    //    });
+    // }
 
 
 
